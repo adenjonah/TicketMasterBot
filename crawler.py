0103c4db775a33e96c591ea04ec82c6097c30270
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2025-01-10 18:37:54 -0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,11 +2,15 @@ import asyncio
 from tasks.fetch_and_process import fetch_events
 from config.db_pool import initialize_db_pool, close_db_pool
 from config.config import DATABASE_URL
+from database.init import initialize_db
 from config.logging import logger
 
 async def main():
     """Main function for the API crawler."""
     try:
+        logger.info("Initializing database...")
+        await initialize_db()
+        logger.info("Database initialized.")
         logger.info("Initializing database pool...")
         await initialize_db_pool(DATABASE_URL)
         logger.info("Database pool initialized.")
